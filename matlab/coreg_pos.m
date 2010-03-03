5492266d64c7a9d82a2eb04f4024d12e858c4a38
Andy Hooper <A.Hooper@leeds.ac.uk>
2010-03-03 16:50:17 +0000

    Make number of coregistration windows scale-independent

--- matlab/coreg_pos.m
+++ matlab/coreg_pos.m
@@ -8,15 +8,14 @@ function []=coreg_pos(firstL,lastL,firstP,lastP,transL,transP)
 % 07/2006 AH: changed spacing of windows to give same number (3600) 
 %              independent of image size
 % 11/2009 AH: code to use translations put back in 
+% 03/2010 AH: revert to just 3600 windows
 % ======================================================================
 
-
-x=round(linspace(firstP+64,lastP-64,30));
-y=round(linspace(firstL+64,lastL-64,120));
-x=[firstP+64+transP:32:lastP-64+transP];
-y=[firstL+64+transL:32:lastL-64+transL];
+x=round(linspace(firstP+64+transP,lastP-64+transP,30));
+y=round(linspace(firstL+64+transL,lastL-64+transL,120));
 [Y,X]=meshgrid(y,x);
 
 fid=fopen('fc_pos.in','w');
 fprintf(fid,'%i  %i\n',[Y(:),X(:)]');
 fclose(fid);
+
