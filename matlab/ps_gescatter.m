2b21f3ec128dff01c6179360cf4edf279b883fef
Mahmut Arikan <mahmut.arikan@gmail.com>
2010-11-22 14:57:14 +0000

    update: ps_gescatter

--- matlab/ps_gescatter.m
+++ matlab/ps_gescatter.m
@@ -1,15 +1,15 @@
-function ps_gescatter(filename,data,step)
-% Generate kml form PS results
+function ps_gescatter(filename,data,step,opacity)
+% Generate kml from PS results
 %
 %
 %
-%   Andy Hooper, June 2006
+%   Andy Hooper, June 2010
 %
 %   ======================================================================
 %   10/2010 MA: Initial version
 %   ======================================================================
 
-% TODO
+% TODO list
 % 1. quadtree zoom levels
 % 2. attach TS series for pixels
 % 3. add colorbar
@@ -21,15 +21,21 @@ function ps_gescatter(filename,data,step)
 %load ps_plot_v-d  ph_disp 
 %data= ph_disp;
 
+if nargin < 4
+  opacity=0.4;
+end
+
+step   % disseminate data
 if nargin < 3
   step=1;
 end
 step   % disseminate data
 
+disp('Loading lotlat matrix...')
 load ps2 lonlat
 
 step=1:step:size(lonlat,1);
 
-gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap','jet','opacity',0.4) 
+gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap','jet','opacity',opacity) 
  
 %EOF
