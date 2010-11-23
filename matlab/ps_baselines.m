e1801e2e45e9cb4a07eee558b1063a76d5af90f3
Andy Hooper <A.Hooper@leeds.ac.uk>
2010-11-23 11:35:08 +0000

    Manual updates

--- matlab/ps_baselines.m
+++ matlab/ps_baselines.m
@@ -1,9 +1,13 @@
 function [bperp,ix]=ps_baselines(max_bperp)
-%PS_BASELINES returns perpendicular baselines below a threshold
+%PS_BASELINES returns perpendicular baselines above a threshold
 %  [BPERP,IX]=PS_BASELINES(MAX_BPERP)
 %
 %   Andy Hooper, July 2009
 %
+%   ======================================================================
+%   11/2010 AH: Return baselines above instead of below threshold
+%   ======================================================================
+
 
 if nargin<1
     max_bperp=inf;
@@ -12,5 +16,5 @@ end
 load psver
 psname=['ps',num2str(psver)];
 load(psname,'bperp');
-ix=find(abs(bperp)<=max_bperp)';
+ix=find(abs(bperp)>=max_bperp)';
 bperp=bperp(ix);
