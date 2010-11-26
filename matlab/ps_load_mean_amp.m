8efa6bf7b4b918234d1a11a719ef0de2571e6ec0
Mahmut Arikan <mahmut.arikan@gmail.com>
2010-11-26 15:16:09 +0000

    ps_load_mean_amp: fix run out of index for first iteration and second iteration over mean_amp.flt

--- matlab/ps_load_mean_amp.m
+++ matlab/ps_load_mean_amp.m
@@ -73,9 +73,14 @@ end
 amp_high=[];
 fid=fopen(ampname,'r');
 while ~feof(fid) % first read through
+%whos
   amp_bit=fread(fid,[width,1000],'float');
   amp_bit(isnan(amp_bit))=1;
   amp_bit=sort(amp_bit(:));
+length(amp_bit)
+  if length(amp_bit)==0  % MA fix for Joana
+    break                % length(amp_bit) returns zero at the end
+  end
   amp_high=[amp_high;amp_bit(round(length(amp_bit)*0.99):end)];
 end
 fclose(fid);
@@ -94,6 +99,9 @@ while ~feof(fid) % second read through
   amp_bit(amp_bit>amp_max)=amp_max;
   amp_bit=uint8(amp_bit*255/amp_max);
   amp_mean=[amp_mean;amp_bit];
+  if length(amp_bit)==0  % MA fix for Joana
+  break
+  end
 end
 
 save(saveampname,'amp_mean');
