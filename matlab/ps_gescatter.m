ea42844640ecc2af6158e707fda69352ae58fbda
Mahmut Arikan <mahmut.arikan@gmail.com>
2012-01-09 13:47:38 +0000

    ps_gescatter - defined color limits

--- matlab/ps_gescatter.m
+++ matlab/ps_gescatter.m
@@ -1,4 +1,4 @@
-function ps_gescatter(filename,data,step,opacity)
+function ps_gescatter(filename,data,step,opacity,climsmtx)
 % Generate kml from PS results
 %
 %
@@ -7,6 +7,7 @@ function ps_gescatter(filename,data,step,opacity)
 %
 %   ======================================================================
 %   10/2010 MA: Initial version
+%   01/2011 MA: define color limits
 %   ======================================================================
 
 % TODO list
@@ -25,17 +26,24 @@ if nargin < 4
   opacity=0.4;
 end
 
-step   % disseminate data
 if nargin < 3
   step=1;
 end
-step   % disseminate data
+step   % print increments of disseminated data
 
-disp('Loading lotlat matrix...')
+disp('Loading lonlat matrix...')
 load ps2 lonlat
 
 step=1:step:size(lonlat,1);
 
-gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap','jet','opacity',opacity) 
+%gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap','jet','opacity',opacity) 
+
+jetme=fliplr(jet);  % change colors order for climsmtx
+
+if (nargin > 4) % do clims
+  gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap',jetme,'opacity',opacity,'clims',climsmtx) 
+else            % don't do clims
+  gescatter(filename,lonlat(step,1),lonlat(step,2),data(step,:),'size',5,'colormap','jet','opacity',opacity)
+end
  
 %EOF
