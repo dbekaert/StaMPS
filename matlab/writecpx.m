e65e409b519b501cb4211761b27abea86292fa2b
Andy Hooper <A.Hooper@leeds.ac.uk>
2012-12-10 17:45:17 +0000

    allow short int files

--- matlab/writecpx.m
+++ matlab/writecpx.m
@@ -1,14 +1,18 @@
-function []=writecpx(fname,vname,precision)
+function []=writecpx(fname,vname,precision,endian)
 
 if nargin < 2
-  error('syntax is writecpx(FILE_NAME, VAR_NAME, PRECISION)')
+  error('syntax is writecpx(FILE_NAME, VAR_NAME, PRECISION, ENDIAN)')
 end
 
 if nargin<3
   precision='float';
 end
 
-fid=fopen(fname,'w');
+if nargin<4
+    endian='n';
+end
+
+fid=fopen(fname,'w',endian);
 
 vname_flt=zeros(size(vname,1),size(vname,2)*2);
 vname_flt(:,1:2:end)=real(vname);
