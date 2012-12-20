679ef4774c9c3adbcf46ff1bfa3285b833707e03
David Bekaert <bekaertdavid@gmail.com>
2012-12-20 11:35:39 +0000

    added ifgs standard deviations as output. [DB]

--- matlab/sb_info.m
+++ matlab/sb_info.m
@@ -2,16 +2,27 @@ function []=sb_info()
 %SB_INFO display small baseline information on the dataset
 %
 %   Andy Hooper, January 2010
+% Modifications:
+% 12/2012   DB:     Add std information as output
 
 load psver
 psname=['ps',num2str(psver)];
-
 ps=load(psname);
+
+% loading the std information
+ifgstdname=['ifgstd',num2str(psver)];
+if exist([ifgstdname,'.mat'],'file')
+    stdin=load(ifgstdname);
+    ifg_std=stdin.ifg_std;
+    clear stdin
+else
+    ifg_std=zeros(ps.n_ifg,1);
+end
 
 if isfield(ps,'ifgday')
     for i=1:ps.n_ifg
         aa=[datestr(ps.ifgday(i,1)),' to ',datestr(ps.ifgday(i,2))];
-        fprintf('%3s  %s %5s m\n',num2str(i),aa,num2str(round(ps.bperp(i))));
+        fprintf('%3s  %s %5s m %.3f deg\n',num2str(i),aa,num2str(round(ps.bperp(i))),ifg_std(i));
     end
     fprintf('Number of stable-phase pixels: %d\n',ps.n_ps);
 else
