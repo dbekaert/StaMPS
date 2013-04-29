f51ddc017198c1c05eaf58ceb3d2479ac623e783
David Bekaert <bekaertdavid@gmail.com>
2013-04-29 14:03:25 +0000

    bandfilter option for ps_plot.

--- matlab/parseplotprm.m
+++ matlab/parseplotprm.m
@@ -6,6 +6,7 @@
 %   11/2010 MA: initial support for 'ts' option 
 %   04/2013 DB: include support of topocorrelated aps 'a_m' for merris, 
 %               'a_l' for linear, and 'a_p' for powerlaw option
+%   04/2013 DB: include bandfilter option 'ifg i'
 
 % list of arguments for ps_plot excluding value type
 arglist={'plot_flag','lims','ref_ifg','ifg_list','n_x','cbar_flag',...
@@ -18,8 +19,13 @@ arglist={'plot_flag','lims','ref_ifg','ifg_list','n_x','cbar_flag',...
 Noptargin = length(varargin(:));  % treat extra options as varargin, for ps_plotTS('v-d',4,'ts') is 3
 
 % Defaults, later below update with user input.
-ts_flag=0;
-aps_flag=0;
+ts_flag=0;              % when 1 do a time-series display
+aps_flag=0;             % when 0 default aps estimation 
+                        % when 1 linear correction aps
+                        % when 2 powerlaw correction aps
+                        % when 3 meris correction aps
+aps_band_flag=0;        % when 1 plot the aps estimated band frequencies
+ifg_number = 0;         % the ifg number for the aps_band_flag
 plot_flag=1; 
 
 % search for char parameter like 'ts','a_l','a_m','a_p'
@@ -41,9 +47,19 @@ for k=1:Noptargin,
        % aps topo correlated meris correction
        aps_flag=3;
        prmsrch=logical([prmsrch strcmp(varargin{k},'a_m') ]);
+       
+       
+   elseif size(varargin{k},2)>3  && strcmp(varargin{k}(1:3),'ifg')==1
+           % display all bands for 'a_p' option for the ith interferogram
+           aps_band_flag=1;
+           ifg_number = str2num(varargin{k}(5:end));
+           prmsrch=logical([prmsrch strcmp(varargin{k}(1:3),'ifg') ]);   
    else
        prmsrch=logical([prmsrch 0]);
-   end      
+   end     
+   
+   
+   
 end
 
 if sum(prmsrch) >= 1
