094fb12c1e9f669f3ad29f153cbdd56a785a38c5
David Bekaert <bekaertdavid@gmail.com>
2014-04-24 20:13:19 +0000

    oscialtor drift correction for envisat

---
+++ matlab/env_oscilator_corr.m
@@ -0,0 +1,96 @@
+function [oscilatior_corr_ifgs,oscilatior_corr_velocity] = env_oscilator_corr(envisat_flag,forced_sm_flag)
+% This function perform oscialtor drift correction for envisat interferograms based on
+% Petar Marinkovic presentation at ESA Living Planet 2013 in Edinburgh.
+% Approximation formula:
+% dR/year = c/2 * (slantRangeTime_FAR - slantRangeTime_NEAR) * corrPerYear
+% 'Apparent displacement' correction:
+% dR/year ~ (7.8m * 5000)*3.87e-7 ~ 0.01482m
+% Correction uses the pixel information in range and in adition for
+% interferograms the temporal baseline information
+%
+% OPTIONAL INPUT:
+% envisat_flag                  'y' when envisat or 'n'. When empty, a
+%                               search is performed in the master.res file
+%
+% OUTPUTS:
+% oscilatior_corr_velocity      Correction in mm for the velocity
+% oscilatior_corr_ifgs          Correction for individual interferograms in rad
+%
+% Correction is defined such that:
+% Corrected interferogram/velocity = original interferogram/velocity - correction
+%
+% P. Marinkovic Envisat oscialtor drift correction coded by David Bekaert -- University of Leeds 2014
+% 
+% cite as:
+% P. Marinkovic (PPO.labs) and Y. Larsen (NORUT)
+% Consequences of Long-Term ASAR Local Oscillator Frequency Decay - an Empirical Study of 10 Years of Data 
+% ESA Living Planet Symposium (2013)
+%
+% Modifications
+% 04/2014       DB      Put non-envisat fix to zeros
+% 04/2014       DB      Allow forced SM oscialtor drift computation
+
+if nargin<1 || isempty(envisat_flag)
+    % checking if this is envisat or not
+    if exist('master.res','file')==2
+        master_file = 'master.res';
+    elseif exist('../master.res','file')==2
+        master_file = '../master.res';
+    else
+        master_file = [];
+    end
+    
+    if ~isempty(master_file)
+        [temp, output] = system(['grep Product\ type\ specifier: ' master_file]);
+        if ~isempty(findstr(output, 'ASAR'))
+            envisat_flag = 'y';
+            fprintf('This is Envisat \n')
+        end
+    else
+        envisat_flag = 'n';
+        fprintf('This is not Envisat \n')
+    end
+end
+
+small_baseline_flag = getparm('small_baseline_flag');
+if nargin<2 
+    forced_sm_flag=0;
+end
+
+if forced_sm_flag==1
+    small_baseline_flag='n';
+end
+
+
+
+if strcmp(envisat_flag,'y')
+    load psver
+    % use the ps2.mat data
+    ps = load(['ps' num2str(psver) '.mat']);
+    lambda = getparm('lambda');
+
+    
+    % velocity map correction:
+    envisat_resolution = 7.8;                   % ground range resolution [m]
+    Oscilator_drift_corr_year = 3.87e-7;         % drift correction in range [1/year]      
+
+    % velocity correction in mm
+    oscilatior_corr_velocity = (envisat_resolution*ps.ij(:,3))*Oscilator_drift_corr_year*1000;
+
+    % interferogra
+    if strcmp(small_baseline_flag,'y')
+        n_ifg = ps.n_ifg;
+        delta_year = (ps.ifgday(:,2)-ps.ifgday(:,1))./365.25;
+    else
+        n_ifg = ps.n_image;
+        delta_year = (ps.day-ps.master_ix)./365.25;
+    end
+    oscilatior_corr_ifgs = -4.*pi./lambda.*repmat(oscilatior_corr_velocity,1,n_ifg)./1000.*repmat(delta_year',ps.n_ps,1);
+
+    
+else
+    load psver
+    ps = load(['ps' num2str(psver) '.mat']);
+    oscilatior_corr_ifgs = zeros([ps.n_ps ps.n_ifg]);
+    oscilatior_corr_velocity = zeros([ps.n_ps 1]);
+end
