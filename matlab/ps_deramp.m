fcdcc4e9655bfd643de388db5c2c35d62af33748
David Bekaert <bekaertdavid@gmail.com>
2017-01-10 14:40:11 +0000

    also hard code the case for a 2nd degree plane correction default is set to degree 1.

--- matlab/ps_deramp.m
+++ matlab/ps_deramp.m
@@ -9,9 +9,12 @@ function [ph_all,ph_ramp] = ps_deramp(ps,ph_all)
 % 11/2013   DB  Fix such deramped SM from SB inversion works as well
 % 12/2013   DB  Add the ramp as output as well
 % 05/2015   DB  Remove warning outputed to user
-% 04/2016   DB  Make sure to use double to avoid machine precision warnings
+% 12/2016   DB  Make sure a double is used for precision issues
+% 01/2017   DB  Also allow for a 2nd order plane correction
 
 fprintf(['Deramping computed on the fly. \n'])
+
+degree = 1;
 
 % SM from SB inversion deramping
 if ps.n_ifg ~= size(ph_all,2)
@@ -19,12 +22,21 @@ if ps.n_ifg ~= size(ph_all,2)
 end
 
 % detrenting of the data
-A = [ps.xy(:,2:3)/1000 ones([ps.n_ps 1])];
+if degree == 1
+    % z = ax + by+ c
+    A = double([ps.xy(:,2:3)/1000 ones([ps.n_ps 1])]);    
+    fprintf('**** z = ax + by+ c\n')
+elseif degree ==2
+    % z = ax^2 + by^2 + cxy + d
+    A = double([(ps.xy(:,2:3)/1000).^2 ps.xy(:,2)/1000.*ps.xy(:,3)/1000 ones([ps.n_ps 1])]);
+    fprintf('**** z = ax^2 + by^2 + cxy + d \n')
+end
+
 ph_ramp = NaN(size(ph_all));
 for k=1:ps.n_ifg
     ix = isnan(ph_all(:,k));
     if ps.n_ps-sum(ix)>5
-        coeff = lscov(double(A(~ix,:)),double(ph_all(~ix,k)));
+        coeff = lscov(A(~ix,:),ph_all(~ix,k));
         ph_ramp(:,k)=A*coeff;
         ph_all(:,k)=ph_all(:,k)-ph_ramp(:,k);
     else
