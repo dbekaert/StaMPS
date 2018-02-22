e2203a16265cc21dea89b232139a0e469ebb17f8
David <David.Bekaert@jpl.nasa.gov>
2018-02-22 09:28:43 -0800

    adding narr and backwards compatibility back in

--- matlab/parseplotprm.m
+++ matlab/parseplotprm.m
@@ -16,6 +16,11 @@
 %   03/2014 DB: Fix for a bug introduced when APS option does not exist.
 %               Add the missing option too  
 %   08/2016 AH: Remove "break" that is incompatible with Matlab2015
+%   11/2017 DB: Change the APS naming convention to allow more models, add
+%               merra and merra2 models
+%   11/2017 DB: Adding the GACOS model 
+%   02/2018 DB: Adding the NARR model and adding in backward compatibility
+
 
 % list of arguments for ps_plot excluding value type
 arglist={'plot_flag','lims','ref_ifg','ifg_list','n_x','cbar_flag',...
@@ -49,130 +54,165 @@ for k=1:Noptargin,
        % time series plot
        ts_flag=1;   
        prmsrch=logical([prmsrch strcmp(varargin{k},'ts') ]);
-   elseif strcmp(varargin{k},'a_l')==1
+   elseif strcmp(varargin{k},'a_linear')==1 || strcmp(varargin{k},'a_l')==1
        % aps topo correlated linear correction
        aps_flag=1;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_l') ]);
-   elseif strcmp(varargin{k},'a_p')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_linear') ]);
+   elseif strcmp(varargin{k},'a_powerlaw')==1 || strcmp(varargin{k},'a_p')==1
        % aps topo correlated powerlaw correction
        aps_flag=2;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_p') ]);
-   elseif strcmp(varargin{k},'a_m')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_powerlaw') ]);
+   elseif strcmp(varargin{k},'a_meris')==1 ||  strcmp(varargin{k},'a_m')==1
        % aps topo correlated meris correction
        aps_flag=3;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_m') ]);
-   elseif strcmp(varargin{k},'a_e')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris') ]);
+   elseif strcmp(varargin{k},'a_erai')==1 || strcmp(varargin{k},'a_e')==1
        % aps topo correlated ERA-I  correction
        aps_flag=4;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_e') ]);
-   elseif strcmp(varargin{k},'a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_erai') ]);
+   elseif strcmp(varargin{k},'a_erai-h')==1 || strcmp(varargin{k},'a_eh')==1
        % aps hydrostatic ERA-I correction
        aps_flag=5;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_eh') ]);
-   elseif strcmp(varargin{k},'a_ew')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_erai-h') ]);
+   elseif strcmp(varargin{k},'a_erai-w')==1 ||  strcmp(varargin{k},'a_ew')==1
        % aps topo correlated ERA-I correction
        aps_flag=6;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_ew') ]);
-   elseif strcmp(varargin{k},'a_w')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_erai-w') ]);
+   elseif strcmp(varargin{k},'a_wrf')==1 ||  strcmp(varargin{k},'a_w')==1
        % aps topo correlated WRF correction
        aps_flag=7;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_w') ]);
-   elseif strcmp(varargin{k},'a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_wrf') ]);
+   elseif strcmp(varargin{k},'a_wrf-h')==1 || strcmp(varargin{k},'a_wh')==1
        % aps hydrostatic WRF correction
        aps_flag=8;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_wh') ]);
-   elseif strcmp(varargin{k},'a_ww')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_wrf-h') ]);
+   elseif strcmp(varargin{k},'a_wrf-w')==1 ||  strcmp(varargin{k},'a_ww')==1
        % aps topo correlated WRF correction
        aps_flag=9;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_ww') ]);
-   elseif strcmp(varargin{k},'a_mi')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_wrf-w') ]);
+   elseif strcmp(varargin{k},'a_meris-ni')==1 ||  strcmp(varargin{k},'a_mi')==1
        % aps topo correlated  MERIS (non-interpolated)
        aps_flag=10;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_mi') ]);     
-  elseif strcmp(varargin{k},'a_pk')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris-ni') ]);     
+  elseif strcmp(varargin{k},'a_powerlaw-k')==1 ||  strcmp(varargin{k},'a_pk')==1
        % Spatial maps of the coefficient relating phase and tropo for power-law
        aps_flag=11;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_pk') ]);   
- elseif strcmp(varargin{k},'a_M')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_powerlaw-k') ]);   
+ elseif strcmp(varargin{k},'a_modis')==1 || strcmp(varargin{k},'a_M')==1
        % aps topo correlated modis correction
        aps_flag=12;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_M') ]);       
- elseif strcmp(varargin{k},'a_MI')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis') ]);       
+ elseif strcmp(varargin{k},'a_modis-ni')==1 || strcmp(varargin{k},'a_MI')==1
        % aps topo correlated modis (non-interpolated)
        aps_flag=13;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_MI') ]);
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis-ni') ]);
        
-  elseif strcmp(varargin{k},'a_m+a_eh')==1
+  elseif strcmp(varargin{k},'a_meris+a_erai-h')==1 ||  strcmp(varargin{k},'a_m+a_eh')==1
        % aps topo correlated MERIS plus a hydrostatic component from ERA-I
        aps_flag=14;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_m+a_eh')]);     
-  elseif strcmp(varargin{k},'a_mi+a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris+a_erai-h')]);     
+  elseif strcmp(varargin{k},'a_meris-ni+a_erai-h')==1 || strcmp(varargin{k},'a_mi+a_eh')==1
        % aps topo correlated MERIS (non-interpolated) plus a hydrostatic component from ERA-I
        aps_flag=15;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_mi+a_eh')]);     
-  elseif strcmp(varargin{k},'a_M+a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris-ni+a_erai-h')]);     
+  elseif strcmp(varargin{k},'a_modis+a_erai-h')==1 || strcmp(varargin{k},'a_M+a_eh')==1
        % aps topo correlated modis plus a hydrostatic component from ERA-I
        aps_flag=16;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_M+a_eh')]);     
-  elseif strcmp(varargin{k},'a_MI+a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis+a_erai-h')]);     
+  elseif strcmp(varargin{k},'a_modis-ni+a_erai-h')==1 || strcmp(varargin{k},'a_MI+a_eh')==1
        % aps topo correlated modis (non-interpolated) plus a hydrostatic component from ERA-I
        aps_flag=17;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_MI+a_eh')]);    
-  elseif strcmp(varargin{k},'a_lman')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis-ni+a_erai-h')]);    
+  elseif strcmp(varargin{k},'a_linear-man')==1 || strcmp(varargin{k},'a_lman')==1
        % aps topo correlated linear manual
        aps_flag=18;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_lman')]);    
- elseif strcmp(varargin{k},'a_RM')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_linear-man')]);    
+ elseif strcmp(varargin{k},'a_recalmodis')==1 || strcmp(varargin{k},'a_RM')==1
        % aps topo correlated modis recalibrated correction
        aps_flag=19;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RM') ]);       
- elseif strcmp(varargin{k},'a_RMI')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis') ]);       
+ elseif strcmp(varargin{k},'a_recalmodis-ni')==1 ||  strcmp(varargin{k},'a_RMI')==1
        % aps topo correlated modis recalibrated (non-interpolated)
        aps_flag=20;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RMI') ]);
-  elseif strcmp(varargin{k},'a_RM+a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis-ni') ]);
+  elseif strcmp(varargin{k},'a_recalmodis+a_erai-h')==1 || strcmp(varargin{k},'a_RM+a_eh')==1
        % aps topo correlated modis recalibrated plus a hydrostatic component from ERA-I
        aps_flag=21;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RM+a_eh')]);     
-  elseif strcmp(varargin{k},'a_RMI+a_eh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis+a_erai-h')]);     
+  elseif strcmp(varargin{k},'a_recalmodis-ni+a_erai-h')==1 || strcmp(varargin{k},'a_RMI+a_eh')==1
        % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from ERA-I
        aps_flag=22;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RMI+a_eh')]);    
-       
-  elseif strcmp(varargin{k},'a_m+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis-ni+a_erai-h')]);    
+  elseif strcmp(varargin{k},'a_meris+a_wrf-h')==1 || strcmp(varargin{k},'a_m+a_wh')==1
        % aps topo correlated MERIS plus a hydrostatic component from WRF
        aps_flag=23;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_m+a_wh')]);     
-   elseif strcmp(varargin{k},'a_mi+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris+a_wrf-h')]);     
+   elseif strcmp(varargin{k},'a_meris-ni+a_wrf-h')==1  || strcmp(varargin{k},'a_mi+a_wh')==1
        % aps topo correlated MERIS (non-interpolated) plus a hydrostatic component from WRF
        aps_flag=24;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_mi+a_wh')]);     
-   elseif strcmp(varargin{k},'a_M+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_meris-ni+a_wrf-h')]);     
+   elseif strcmp(varargin{k},'a_modis+a_wrf-h')==1 ||  strcmp(varargin{k},'a_M+a_wh')==1
        % aps topo correlated modis plus a hydrostatic component from  WRF
        aps_flag=25;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_M+a_wh')]);     
-   elseif strcmp(varargin{k},'a_MI+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis+a_wrf-h')]);     
+   elseif strcmp(varargin{k},'a_modis-ni+a_wrf-h')==1 || strcmp(varargin{k},'a_MI+a_wh')==1
        % aps topo correlated modis (non-interpolated) plus a hydrostatic component from WRF
        aps_flag=26;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_MI+a_wh')]);    
-   elseif strcmp(varargin{k},'a_RM+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_modis-ni+a_wrf-h')]);    
+   elseif strcmp(varargin{k},'a_recalmodis+a_wrf-h')==1 || strcmp(varargin{k},'a_RM+a_wh')==1
        % aps topo correlated modis recalibrated plus a hydrostatic component from WRF
        aps_flag=27;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RM+a_wh')]);     
-   elseif strcmp(varargin{k},'a_RMI+a_wh')==1
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis+a_wrf-h')]);     
+   elseif strcmp(varargin{k},'a_recalmodis-ni+a_wrf-h')==1 || strcmp(varargin{k},'a_RMI+a_wh')==1
        % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
        aps_flag=28;
-       prmsrch=logical([prmsrch strcmp(varargin{k},'a_RMI+a_wh')]);   
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_recalmodis-ni+a_wrf-h')]);  
+  elseif strcmp(varargin{k},'a_merra')==1
+        % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+        aps_flag=29;
+        prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra')]);     
+   elseif strcmp(varargin{k},'a_merra2')==1
+       % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+       aps_flag=30;
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra2')]); 
+   elseif strcmp(varargin{k},'a_merra-h')==1
+       % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+       aps_flag=31;
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra-h')]); 
+   elseif strcmp(varargin{k},'a_merra2-h')==1
+       % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+       aps_flag=32;   
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra2-h')]); 
+  elseif strcmp(varargin{k},'a_merra-w')==1
+       % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+       aps_flag=33;
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra-w')]); 
+   elseif strcmp(varargin{k},'a_merra2-w')==1
+       % aps topo correlated modis recalibrated (non-interpolated) plus a hydrostatic component from WRF
+       aps_flag=34; 
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_merra2-w')]);   
+   elseif strcmp(varargin{k},'a_gacos')==1
+       % aps topo correlated GACOS  
+       aps_flag=35; 
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_gacos')]);    
+  elseif strcmp(varargin{k},'a_narr')==1
+       % aps topo correlated NARR  
+       aps_flag=36; 
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_narr')]);  
+  elseif strcmp(varargin{k},'a_narr-h')==1
+       % aps topo correlated NARR  
+       aps_flag=37; 
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_narr-h')]); 
+  elseif strcmp(varargin{k},'a_narr-w')==1
+       % aps topo correlated NARR  
+       aps_flag=38; 
+       prmsrch=logical([prmsrch strcmp(varargin{k},'a_narr-w')]);        
 
-   elseif strcmp(varargin{k},'i_as')==1
+  elseif strcmp(varargin{k},'i_as')==1
        % ionopsheric correlated atmopshere - azimuth shift method
        iono_flag=1;
        prmsrch=logical([prmsrch strcmp(varargin{k},'i_as')]);   
  
-       
-       
-    
-       
        
  elseif size(varargin{k},2)>=3  && strcmp(varargin{k}(1:3),'ifg')==1
            if size(varargin{k},2)==3
